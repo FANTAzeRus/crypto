52fd8c71825018032393490f619c2baefe669ddd
Fedor A. Nokhrin <fedor.nohrin@gmail.com>
2020-12-21 14:11:51 +0300

    Fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,4 @@
 use App\Http\Controllers\StatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/add_stat', [StatController::class, 'generateNewRecord']);
-Route::view('/test-route', 'addstat');
-
 Route::view('/{any}', 'welcome')->where('any', '.*');
